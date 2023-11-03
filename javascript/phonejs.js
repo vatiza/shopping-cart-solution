9d47ced054c7eb9ed1a28aa93d2db79c766e6acc
vatiza <rakibur.mac@gmail.com>
2023-11-03 22:36:50 +0600

    update quantity

--- javascript/phonejs.js
+++ javascript/phonejs.js
@@ -1,47 +1,62 @@
-function iphoneQuantiyBtn(isIncrease){
-    const quantitynumber=document.getElementById('quantity');
-    const quantityTOnumber=quantitynumber.value;
-    const PreviousstringToNumber=parseInt(quantityTOnumber);
+function iphoneQuantiyBtn(isIncrease) {
+    const quantitynumber = document.getElementById('quantity');
+    const quantityTOnumber = quantitynumber.value;
+    const PreviousstringToNumber = parseInt(quantityTOnumber);
     let newPhoneNumber;
-    if(isIncrease===true){
-       newPhoneNumber=PreviousstringToNumber +1;
+    if (isIncrease === true) {
+        newPhoneNumber = PreviousstringToNumber + 1;
     }
-else{
-        newPhoneNumber=PreviousstringToNumber-1;
+    else {
+        newPhoneNumber = PreviousstringToNumber - 1;
     }
-    quantitynumber.value=newPhoneNumber;
+    quantitynumber.value = newPhoneNumber;
     return newPhoneNumber;
 }
 
-function updateTotalPhonePrice(newPhoneNumber){
-    const totalPhoneQuantityPrice= newPhoneNumber*1219;
-const priceElement=document.getElementById('total-quantity-amount');
-    priceElement.innerText=totalPhoneQuantityPrice;
+function updateTotalPhonePrice(newPhoneNumber) {
+    const totalPhoneQuantityPrice = newPhoneNumber * 1219;
+    const priceElement = document.getElementById('total-quantity-amount');
+    priceElement.innerText = totalPhoneQuantityPrice;
 }
 
-document.getElementById('iphone-plush-btn').addEventListener('click',function(){
-   const newPhoneNumber= iphoneQuantiyBtn(true);
+document.getElementById('iphone-plush-btn').addEventListener('click', function () {
+    const newPhoneNumber = iphoneQuantiyBtn(true);
 
-   updateTotalPhonePrice(newPhoneNumber);
+    updateTotalPhonePrice(newPhoneNumber);
 });
-document.getElementById('iphone-minus-btn').addEventListener('click',function(){
-   const newPhoneNumber=  iphoneQuantiyBtn(false);
-   updateTotalPhonePrice(newPhoneNumber);
+document.getElementById('iphone-minus-btn').addEventListener('click', function () {
+    const newPhoneNumber = iphoneQuantiyBtn(false);
+    updateTotalPhonePrice(newPhoneNumber);
 })
 // !----------------------End phone section ------------------------------------------------
 
 // !------------------------Start Case section ---------------------------------------------
-function phoneCaseupdate(){
-    const getQuantityfield=document.getElementById('case-quantity');
-    const getQuantityValue=getQuantityfield.value;
-    const PreviousQuantityNumber=parseInt(getQuantityValue);
+function phoneCaseupdate(isIncrease) {
+    const getQuantityfield = document.getElementById('case-quantity');
+    const getQuantityValue = getQuantityfield.value;
+    const PreviousQuantityNumber = parseInt(getQuantityValue);
     let newCaseQuantity;
-if(isIncrease===true){
-    newCaseQuantity=PreviousQuantityNumber+1;
-}else{
-    newCaseQuantity=PreviousQuantityNumber-1;
+    if (isIncrease === true) {
+        newCaseQuantity = PreviousQuantityNumber + 1;
+    } else {
+        newCaseQuantity = PreviousQuantityNumber - 1;
+    }
+    getQuantityfield.value = newCaseQuantity;
+    return newCaseQuantity;
 }
-getQuantityfield.value=newCaseQuantity;
-return newCaseQuantity
+function updateTotalCasePrice(newCaseQuantity) {
+    const totalCaseQuantityPrice = newCaseQuantity * 59;
+    const priceElement = document.getElementById('quantity-price');
+    priceElement.innerText = totalCaseQuantityPrice;
+}
 
-}
+document.getElementById('case-plus').addEventListener("click", function () {
+    const newCaseQuantity = phoneCaseupdate(true);
+
+    updateTotalCasePrice(newCaseQuantity);
+})
+document.getElementById('case-minus').addEventListener("click", function () {
+    const newCaseQuantity = phoneCaseupdate(false);
+
+    updateTotalCasePrice(newCaseQuantity);
+})
