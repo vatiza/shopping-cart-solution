9768379c8d6cfe72cfd6a29e43d18ca14ca86813
vatiza <rakibur.mac@gmail.com>
2023-11-03 22:19:57 +0600

    case section

--- javascript/phonejs.js
+++ javascript/phonejs.js
@@ -1,35 +1,47 @@
-
 function iphoneQuantiyBtn(isIncrease){
     const quantitynumber=document.getElementById('quantity');
     const quantityTOnumber=quantitynumber.value;
     const PreviousstringToNumber=parseInt(quantityTOnumber);
+    let newPhoneNumber;
     if(isIncrease===true){
-        const increaseAddNewQuantiy=PreviousstringToNumber +1;
-        quantitynumber.value=increaseAddNewQuantiy;
+       newPhoneNumber=PreviousstringToNumber +1;
     }
-    if(isIncrease===false){
-        if(PreviousstringToNumber>0){
-            const dicreaseTheQuantity=PreviousstringToNumber-1;
-            quantitynumber.value=dicreaseTheQuantity;
-        }
-       
+else{
+        newPhoneNumber=PreviousstringToNumber-1;
     }
+    quantitynumber.value=newPhoneNumber;
+    return newPhoneNumber;
 }
 
-
+function updateTotalPhonePrice(newPhoneNumber){
+    const totalPhoneQuantityPrice= newPhoneNumber*1219;
+const priceElement=document.getElementById('total-quantity-amount');
+    priceElement.innerText=totalPhoneQuantityPrice;
+}
 
 document.getElementById('iphone-plush-btn').addEventListener('click',function(){
-    iphoneQuantiyBtn(true);
+   const newPhoneNumber= iphoneQuantiyBtn(true);
+
+   updateTotalPhonePrice(newPhoneNumber);
 });
 document.getElementById('iphone-minus-btn').addEventListener('click',function(){
-    iphoneQuantiyBtn(false);
-    })
+   const newPhoneNumber=  iphoneQuantiyBtn(false);
+   updateTotalPhonePrice(newPhoneNumber);
+})
+// !----------------------End phone section ------------------------------------------------
 
-// document.getElementById('iphone-minus-btn').addEventListener('click',function(){
-// const quantityMinus=document.getElementById('quantity');
-// const getTheQuantityvalue=quantityMinus.value;
-// const PreviousstringToNumber=parseInt(getTheQuantityvalue);
-// const dicreaseTheQuantity=PreviousstringToNumber-1;
-// quantityMinus.value=dicreaseTheQuantity;
+// !------------------------Start Case section ---------------------------------------------
+function phoneCaseupdate(){
+    const getQuantityfield=document.getElementById('case-quantity');
+    const getQuantityValue=getQuantityfield.value;
+    const PreviousQuantityNumber=parseInt(getQuantityValue);
+    let newCaseQuantity;
+if(isIncrease===true){
+    newCaseQuantity=PreviousQuantityNumber+1;
+}else{
+    newCaseQuantity=PreviousQuantityNumber-1;
+}
+getQuantityfield.value=newCaseQuantity;
+return newCaseQuantity
 
-// })
+}
